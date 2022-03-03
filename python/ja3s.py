80b7ce49b1b275a5fa1e4c21cf7b965d4fe5a782
thegwan <gwan.email@gmail.com>
2022-03-03 12:35:49 -0800

    use integer ID code of chosen ciphersuite instead of the object name

--- python/ja3s.py
+++ python/ja3s.py
@@ -131,7 +131,7 @@ def process_pcap(pcap, any_port=False):
             ja3 = [str(server_handshake.version)]
 
             # Cipher Suites (16 bit values)
-            ja3.append(str(server_handshake.ciphersuite))
+            ja3.append(str(server_handshake.ciphersuite.code))
             ja3 += process_extensions(server_handshake)
             ja3 = ",".join(ja3)
 
